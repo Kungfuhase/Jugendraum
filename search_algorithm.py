1002f38494d2d86ea6322a53aa8d59f4f686fa8d
KixxCC <41233933+KixxCC@users.noreply.github.com>
2018-09-05 22:19:13 +0200

    version 0.1

--- search_algorithm.py
+++ search_algorithm.py
@@ -9,7 +9,7 @@ def search_word_txt(file, words):
             while not done:
                 if word in file[i][start_i:]:
                     index = file[i].find(word, start_i)
-                    indices.append(('Row:' + str(i), index, word))
+                    indices.append(('Row: ' + str(i+1) + ' ', 'index: ' + str(index) + ' ','word: '+ word + ' '))
                     start_i += index + len(word)
                 else:
                     done = True
@@ -40,7 +40,7 @@ def search_word_and_x(file, words, length_x):
                     else:
                         following_word = file[i][start_i:start_i + length_x]
                         row = i
-                    indices.append(('Row:' + row, index, word, following_word))
+                    indices.append(('Row:' + str(row+1) + ' ', 'index: '+ str(index) + ' ', 'word: ' + word + ' ', 'following word : '+following_word + ' '))
                 else:
                     done = True
     return indices
@@ -55,7 +55,7 @@ def search_word_string(string, words):
             while not done:
                 if word in string[start_i:]:
                     index = string.find(word, start_i)
-                    indices.append((index, word))
+                    indices.append(('index: ' + str(index) + ' ', 'word: '+ word + ' '))
                     start_i += index + len(word)
                 else:
                     done = True
@@ -71,7 +71,7 @@ def search_word_and_x_string(string, words, length_x):
             if word in string[start_i:]:
                 index = string.find(word, start_i)
                 start_i += index + len(word)
-                indices.append((index, word, string[start_i:start_i + length_x]))
+                indices.append(('index: ' + str(index) + ' ','word: ' + word + ' ', 'following word : ' + string[start_i:start_i + length_x] + ' '))
             else:
                 done = True
     return indices
@@ -90,6 +90,27 @@ def y_n_question (msg="Yes or No?: "):
             print('Please enter y or n')
             continue
     return answer
+
+
+def processing_result (result,source,source_type):
+    for i in range(len(result)):
+        print(result[i])
+    while True:
+        option = input("Do you want to safe this as a file: ")
+        if option.lower() in ['y','yes']:
+            break
+        elif option.lower() in ['n','no']:
+            print("pass this bs")
+            break
+        else:
+            print('Invalid choice type y or n')
+            break
+    if option.lower() in ['y','yes']:
+        path = input('Please enter a path and name to safe the file : ')
+        file = open(path,'w')
+        file.write(source_type + source + '\n')
+        [file.write(str(''.join(result[i]) + '\n')) for i in range(len(result))]
+        file.close()
 
 
 
@@ -118,7 +139,10 @@ if __name__ == '__main__':
                     break
                 except Exception:
                     print("\nThis is an invalid path\n")
-            words_raw = input("Please enter the words, divide them with a ',' no spaces")
+            while True: # we need this in 3 boyyy
+                words_raw = input("Please enter the words, divide them with a ',' no spaces: ")
+                if words_raw:
+                    break
             words = words_raw.split(',')
             if choice == '1':
                 result_tuple = search_word_txt(txt,words)
@@ -134,21 +158,67 @@ if __name__ == '__main__':
                             cont = y_n_question("Continue?: ")
                             if not cont:
                                 choice = 'q'
+                processing_result(result_tuple,path,'file: ')
+
             elif choice == '2':
                 while True:
                     length_following_figures = int(input('Please enter the number of following figures: '))
-                    if length_following_figures <= 0:
+                    if length_following_figures >= 0:
                         break
                     else:
                         print("Invalid length try again")
+                        continue
                 result_tuple = search_word_and_x(txt,words,length_following_figures)
+                if not result_tuple:
+                    check_again = y_n_question("Nothing found but you could check it again (no rows): ")
+                    if check_again:
+                        txt_string = open(path).read()
+                        for i in range(len(txt_string)):
+                            txt_string = txt_string.rstrip('\n')
+                        result_tuple = search_word_and_x_string(txt_string,words,length_following_figures)
+                        if not result_tuple:
+                            print('Still nothing found')
+                            cont = y_n_question("Continue?: ")
+                            if not cont:
+                                choice = 'q'
 
+                processing_result(result_tuple,path,'file: ')
+        elif choice in ['3','4']:
+            while True:
+                been_here = False
+                while not been_here:
+                    string = input("Please enter the string: ")
+                    if not string:
+                        print("\nThis is an invalid string\n")
+                    else:
+                        been_here = True
+                words_raw = input("Please enter the words, divide them with a ',' no spaces: ")
+                if words_raw:
+                    words = words_raw.split(',')
+                    break
+                else:
+                    continue
+            if choice == '3':
+                while True:
+                    length_following_figures = int(input('Please enter the number of following figures: '))
+                    if length_following_figures >= 0:
+                        break
+                    else:
+                        print("Invalid length try again")
+                result_tuple = search_word_and_x_string(string,words,length_following_figures)
+                if result_tuple:
+                    processing_result(result_tuple,string,'file: ')
+                else:
+                    print("Nothing found")
+            if choice == '4':
+                result_tuple = search_word_string(string,words)
+                if result_tuple:
+                    processing_result(result_tuple,string,'file: ')
+                else:
+                    print("Nothing found")
 
-                #HEREEEEEEEEEEE
-
-                pass
         elif choice == 'q' or 'Q':
-            print("Closing the program")
+            print("Exit")
             break
         else:
             print("Invalid choice please try again")
